803d45a5b4e11eca378f00466e557c7bf628b7cd
hossenmaruf <severusmaruf740@gmail.com>
2023-01-18 22:16:27 +0600

    first commit

---
+++ Template.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package  Template
+ */
+/*
+Plugin Name: Template
+Plugin URI: https://github.com/hossenmaruf
+Description: This is my custom Plugin
+Version: 1.0.0
+Author: hossenmaruf
+Author URI: https://github.com/hossenmaruf
+License: GPLv2 or later
+Text Domain: Template
+*/
